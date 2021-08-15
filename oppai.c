7eff77bdf25b7a7159a8d8bc0a911a809548625b
shit <36964527+7ez@users.noreply.github.com>
2021-08-15 09:17:19 +0200

    a little bit more nerfing

--- oppai.c
+++ oppai.c
@@ -682,7 +682,7 @@ int mods_apply(ezpp_t ez) {
 
   /* global multipliers */
   od_ar_hp_multiplier = 1;
-  if (ez->mods & MODS_HR) od_ar_hp_multiplier *= 1.4f;
+  if (ez->mods & MODS_HR) od_ar_hp_multiplier *= 1.2f;
   if (ez->mods & MODS_EZ) od_ar_hp_multiplier *= 0.5f;
 
   ez->od *= od_ar_hp_multiplier;
@@ -702,7 +702,7 @@ int mods_apply(ezpp_t ez) {
     : (5 + (AR5_MS - arms) / AR_MS_STEP2);
 
   cs_multiplier = 1;
-  if (ez->mods & MODS_HR) cs_multiplier = 1.3f;
+  if (ez->mods & MODS_HR) cs_multiplier = 1.1f;
   if (ez->mods & MODS_EZ) cs_multiplier = 0.5f;
   ez->cs *= cs_multiplier;
   ez->cs = al_max(0.0f, al_min(10.0f, ez->cs));
